79e5c3bf03f4cbeed25eee8a5669863013bff477
lucio-araujo <lucio.ribeiroaraujo@outlook.com>
2023-11-18 12:10:47 -0300

    Home and Header components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,16 @@
-export default function Header() {
-  // TODO
+/* eslint-disable prettier/prettier */
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.container}>
+      <Link href="/">
+        <a title="Home">
+          <img src="/logo.svg" alt="logo" />
+        </a>
+      </Link>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable prettier/prettier */
 import { GetStaticProps } from 'next';
+
+import Head from 'next/head';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
-import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
 interface Post {
@@ -24,13 +27,107 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Home | Space Travelling</title>
+      </Head>
+      <main className={styles.container}>
+        <a href="#" className={styles.post} title="Post">
+          <strong>Como utilizar Hooks</strong>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.info}>
+            <span>
+              <FiCalendar size={20} /> 15 Mar 2021
+            </span>
+            <span>
+              <FiUser size={20} /> Joseph Oliveira
+            </span>
+          </div>
+        </a>
+        <a href="#" className={styles.post} title="Post">
+          <strong>Criando um app CRA do zero</strong>
+          <p>
+            Tudo sobre como criar a sua primeira aplicação utilizando Create
+            React App
+          </p>
+          <div className={styles.info}>
+            <span>
+              <FiCalendar size={20} /> 19 Abr 2021
+            </span>
+            <span>
+              <FiUser size={20} /> Danilo Vieira
+            </span>
+          </div>
+        </a>
+        <a href="#" className={styles.post} title="Post">
+          <strong>Como utilizar Hooks</strong>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.info}>
+            <span>
+              <FiCalendar size={20} /> 15 Mar 2021
+            </span>
+            <span>
+              <FiUser size={20} /> Joseph Oliveira
+            </span>
+          </div>
+        </a>
+        <a href="#" className={styles.post} title="Post">
+          <strong>Criando um app CRA do zero</strong>
+          <p>
+            Tudo sobre como criar a sua primeira aplicação utilizando Create
+            React App
+          </p>
+          <div className={styles.info}>
+            <span>
+              <FiCalendar size={20} /> 19 Abr 2021
+            </span>
+            <span>
+              <FiUser size={20} /> Danilo Vieira
+            </span>
+          </div>
+        </a>
+        <a href="#" className={styles.post} title="Post">
+          <strong>Como utilizar Hooks</strong>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.info}>
+            <span>
+              <FiCalendar size={20} /> 15 Mar 2021
+            </span>
+            <span>
+              <FiUser size={20} /> Joseph Oliveira
+            </span>
+          </div>
+        </a>
+        <a href="#" className={styles.post} title="Post">
+          <strong>Criando um app CRA do zero</strong>
+          <p>
+            Tudo sobre como criar a sua primeira aplicação utilizando Create
+            React App
+          </p>
+          <div className={styles.info}>
+            <span>
+              <FiCalendar size={20} /> 19 Abr 2021
+            </span>
+            <span>
+              <FiUser size={20} /> Danilo Vieira
+            </span>
+          </div>
+        </a>
+        <button type="button" className={styles.loadMorePosts}>
+          Carregar mais posts
+        </button>
+      </main>
+    </>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
-
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  // const prismic = getPrismicClient();
+  // const postsResponse = await prismic.query(TODO);
+  // TODO
+  return {
+    props: {},
+  };
+};
